b166f6a989621f1cfe987adaf3b4fd45baa0ca08
Tharnid <jeffmsmithdev@gmail.com>
2022-08-17 22:55:13 -0400

    moving on to next section after trying an ant component

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 
 npm init
 npm install next react react-dom
+npm install antd
+npm install --save @ant-design/icons
+npm run dev
+
+local state
+global state
--- cms/client/pages/index.js
+++ cms/client/pages/index.js
@@ -1,7 +1,10 @@
+import { Button } from 'antd';
+
 function Home() {
     return (
         <div>
             <h1>Home</h1>
+            <Button type="dashed">Dashed Button</Button>
         </div>
    );
 }
